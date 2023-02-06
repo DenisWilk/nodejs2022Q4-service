00ed615b91020dcda6f751b08cbd43f46216da73
Den <>
2023-02-06 20:00:58 +0300

    feat: create app files structure

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const port = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(port, () => console.log(`Server started on port ${port}`));
+  await app.listen(port, () => console.log(`Server started on port = ${port}`));
 }
 bootstrap();
