826065ee76e5d57abbaf0d659e7b0166b9682596
Den <>
2023-02-15 22:45:37 +0300

    feat: implement OpenAPI spec in doc folder

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
 import { AppModule } from './app.module';
 
 const port = Number(process.env.PORT) || 4000;
@@ -11,6 +15,9 @@ async function bootstrap() {
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const api = await readFile(join(__dirname, '../doc/api.yaml'), 'utf-8');
+  SwaggerModule.setup('doc', app, parse(api));
   
   await app.listen(port, () => console.log(`Server started on port = ${port}`));
 }
