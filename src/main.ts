dff901aeb5d5c65bf9e11da9463048c1a44b3c18
Den <>
2023-02-15 22:27:47 +0300

    feat: add data validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 const port = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  
   await app.listen(port, () => console.log(`Server started on port = ${port}`));
 }
 bootstrap();
